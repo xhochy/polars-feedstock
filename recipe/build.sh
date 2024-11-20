38ed55b8f500aff2c42eb8f575900db989d691a4
Uwe Korn <uwe.korn@quantco.com>
2024-11-20 11:41:38 +0000

    Ready for bisecting

--- recipe/build.sh
+++ recipe/build.sh
@@ -70,5 +70,4 @@ fi
 
 # The root level Cargo.toml is part of an incomplete workspace
 # we need to use the manifest inside the py-polars
-cd py-polars
 cargo-bundle-licenses --format yaml --output ../THIRDPARTY.yml
